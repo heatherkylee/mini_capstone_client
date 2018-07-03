f4375c867c135bddf1edcec295cce01af7e4b272
Heather Lee <heather.k.lee@gmail.com>
2018-07-02 22:53:17 -0400

    make show and new routes and view pages

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -5,4 +5,15 @@ class Client::ProductsController < ApplicationController
     @products = response.body
     render "index.html.erb"
   end
+
+  def show
+    product_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/products/#{product_id}")
+    @product = response.body
+    render "show.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
 end
